f7284cff4d0d22cef70ea52973c7db23e7580d3d
Diony Rosa <dhrosa@gmail.com>
2023-05-20 16:15:37 -0400

    Translate fatfs errors to friendly strings.

--- fs.cc
+++ fs.cc
@@ -219,14 +219,67 @@ PARTITION VolToPart[] = {
 /////////
 
 namespace {
-void ThrowIfError(FRESULT result) {
+
+struct Error {
+  std::string_view name;
+  std::errc errc = std::errc::io_error;
+};
+
+Error ToError(FRESULT result) {
+  switch (result) {
+    case FR_DISK_ERR:
+      return {"DISK_ERR"};
+    case FR_INT_ERR:
+      return {"INT_ERR"};
+    case FR_NOT_READY:
+      return {"NOT_READY"};
+    case FR_NO_FILE:
+      return {"NO_FILE", std::errc::no_such_file_or_directory};
+    case FR_NO_PATH:
+      return {"NO_PATH", std::errc::no_such_file_or_directory};
+    case FR_INVALID_NAME:
+      return {"INVALID_NAME", std::errc::invalid_argument};
+    case FR_DENIED:
+      return {"DENIED", std::errc::permission_denied};
+    case FR_EXIST:
+      return {"EXIST", std::errc::file_exists};
+    case FR_INVALID_OBJECT:
+      return {"INVALID_OBJECT", std::errc::invalid_argument};
+    case FR_WRITE_PROTECTED:
+      return {"WRITE_PROTECTED", std::errc::read_only_file_system};
+    case FR_INVALID_DRIVE:
+      return {"INVALID_DRIVE", std::errc::no_such_device};
+    case FR_NOT_ENABLED:
+      return {"NOT_ENABLED", std::errc::no_such_device};
+    case FR_NO_FILESYSTEM:
+      return {"NO_FILESYSTEM", std::errc::no_such_device};
+    case FR_MKFS_ABORTED:
+      return {"MKFS_ABORTED"};
+    case FR_TIMEOUT:
+      return {"TIMEOUT", std::errc::timed_out};
+    case FR_LOCKED:
+      return {"LOCKED", std::errc::no_lock_available};
+    case FR_NOT_ENOUGH_CORE:
+      return {"NOT_ENOUGH_CORE", std::errc::not_enough_memory};
+    case FR_TOO_MANY_OPEN_FILES:
+      return {"TOO_MANY_OPEN_FILES", std::errc::too_many_files_open};
+    case FR_INVALID_PARAMETER:
+      return {"INVALID_PARAMETER", std::errc::invalid_argument};
+    default:
+      return {"UNKNOWN"};
+  }
+}
+
+void ThrowIfError(std::string_view op, FRESULT result) {
   if (result == FR_OK) {
     return;
   }
+  const Error error = ToError(result);
   std::stringstream what;
-  what << "FAT FS error: " << static_cast<int>(result);
-  throw std::filesystem::filesystem_error(
-      what.str(), std::make_error_code(std::errc::io_error));
+  what << op << " error " << error.name << "(" << static_cast<int>(result)
+       << ")";
+  throw std::filesystem::filesystem_error(what.str(),
+                                          std::make_error_code(error.errc));
 }
 }  // namespace
 
@@ -234,9 +287,10 @@ FileSystem::FileSystem() {
   std::cout << "FAT file system initialization start." << std::endl;
   const LBA_t partition_sizes[] = {kSectorCount - 5};
   std::array<BYTE, kSectorSize> work_area;
-  ThrowIfError(f_fdisk(0, partition_sizes, work_area.data()));
-  ThrowIfError(f_mkfs("0:", nullptr, work_area.data(), work_area.size()));
-  ThrowIfError(f_mount(&fs_, "", 1));
+  ThrowIfError("fdisk", f_fdisk(0, partition_sizes, work_area.data()));
+  ThrowIfError("mkfs",
+               f_mkfs("0:", nullptr, work_area.data(), work_area.size()));
+  ThrowIfError("mount", f_mount(&fs_, "", 1));
   std::cout << "FAT file system initialization complete." << std::endl;
   fs_initialized = true;
 }
@@ -259,7 +313,7 @@ File File::Open(std::filesystem::path path, const OpenFlags& flags) {
 
   File file;
   file.fat_file_ = std::make_unique<FIL>();
-  ThrowIfError(f_open(file.fat_file_.get(), path.c_str(), mode));
+  ThrowIfError("open", f_open(file.fat_file_.get(), path.c_str(), mode));
   return file;
 }
 
@@ -275,41 +329,41 @@ File::~File() {
 }
 
 void File::Close() {
-  ThrowIfError(f_close(std::exchange(fat_file_, nullptr).get()));
+  ThrowIfError("close", f_close(std::exchange(fat_file_, nullptr).get()));
 }
 
 int File::Tell() { return f_tell(fat_file_.get()); }
 
 void File::Seek(int location) {
-  ThrowIfError(f_lseek(fat_file_.get(), location));
+  ThrowIfError("lseek", f_lseek(fat_file_.get(), location));
 }
 
 std::span<std::byte> File::Read(std::span<std::byte> buffer) {
   UINT bytes_read;
-  ThrowIfError(
-      f_read(fat_file_.get(), buffer.data(), buffer.size(), &bytes_read));
+  ThrowIfError("read", f_read(fat_file_.get(), buffer.data(), buffer.size(),
+                              &bytes_read));
   return buffer.subspan(bytes_read);
 }
 
 int File::Write(std::span<const std::byte> buffer) {
   UINT bytes_written;
-  ThrowIfError(
-      f_write(fat_file_.get(), buffer.data(), buffer.size(), &bytes_written));
+  ThrowIfError("write", f_write(fat_file_.get(), buffer.data(), buffer.size(),
+                                &bytes_written));
   return bytes_written;
 }
 
-void File::Sync() { ThrowIfError(f_sync(fat_file_.get())); }
+void File::Sync() { ThrowIfError("sync", f_sync(fat_file_.get())); }
 
 Directory Directory::Open(std::filesystem::path path) {
   Directory dir;
   dir.fat_dir_ = std::make_unique<DIR>();
-  ThrowIfError(f_opendir(dir.fat_dir_.get(), path.c_str()));
+  ThrowIfError("opendir", f_opendir(dir.fat_dir_.get(), path.c_str()));
   return dir;
 }
 
 Directory::~Directory() {
   try {
-    ThrowIfError(f_closedir(fat_dir_.get()));
+    ThrowIfError("closedir", f_closedir(fat_dir_.get()));
   } catch (const std::filesystem::filesystem_error& e) {
     std::cout << "Error while closing directory: " << e.what() << std::endl;
   }
@@ -327,10 +381,10 @@ std::vector<Directory::Entry> Directory::Entries() {
   std::vector<Entry> entries;
   FILINFO file_info;
   while (true) {
-    ThrowIfError(f_readdir(fat_dir_.get(), &file_info));
+    ThrowIfError("readdir", f_readdir(fat_dir_.get(), &file_info));
     if (file_info.fname[0] == 0) {
       // End of directory.
-      ThrowIfError(f_rewinddir(fat_dir_.get()));
+      ThrowIfError("rewinddir", f_rewinddir(fat_dir_.get()));
       return entries;
     }
     entries.push_back(ToEntry(file_info));
